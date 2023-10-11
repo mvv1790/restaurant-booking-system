3d1ca6ed70a7f576dfc98c3c8b38c2bd8fc77a73
mvv1790 <mvv1790@gmail.com>
2023-10-11 14:39:54 +0000

    Added models.py

---
+++ booking_project/models.py
@@ -0,0 +1,26 @@
+# booking_app/models.py
+
+from django.db import models
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+
+
+class Table(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    seating_capacity = models.PositiveIntegerField()
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    booking_date = models.DateTimeField()
+    number_of_guests = models.PositiveIntegerField()
+
+
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
