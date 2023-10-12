849dfc3d473f8149640133d6dae51495fe3afb38
mvv1790 <mvv1790@gmail.com>
2023-10-12 11:29:17 +0000

    Fixed urls.py for all pages to load.

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.list_tables, name='list_tables'),
+    path('home/', views.home, name='home'),
     path('book/', views.book_table, name='book_table'),
-    path('menu/', views.view_menu, name='view_menu'),
+    path('menu/', views.view_menu, name='menu'),
 ]
+
--- booking_app/views.py
+++ booking_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 
+def home(request):
+    return render(request, 'home.html')
+
+
 def list_tables(request):
     # Placeholder logic for now
     return render(request, 'booking_app/list_tables.html')
