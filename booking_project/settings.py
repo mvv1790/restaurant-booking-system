87d0bb2ab2ed9e7edb7c1af3d0e4fc469882f08b
mvv1790 <mvv1790@gmail.com>
2023-10-20 08:25:09 +0000

    Added home image and featured dish content

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -148,10 +148,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/assets/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
-]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
