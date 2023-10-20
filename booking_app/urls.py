9e8efab124d58a471344502d8e5af3e9475b7243
mvv1790 <mvv1790@gmail.com>
2023-10-20 06:34:59 +0000

    Fixing issue of images not loading in properly

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.list_tables, name='list_tables'),
@@ -7,4 +9,8 @@ urlpatterns = [
     path('book/', views.book_table, name='book_table'),
     path('menu/<int:table_id>/', views.view_menu, name='menu'),
     path('general_menu/', views.general_menu, name='general_menu'),
-]
+] 
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- booking_project/settings.py
+++ booking_project/settings.py
@@ -146,7 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/assets/"
+STATICFILES_DIRS = [BASE_DIR / "assets",]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
